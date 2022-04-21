dcbf5a0556528849e5160312825f88f974b36950
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-04-21 19:39:30 +0300

    Добавьте файлы проекта.

---
+++ userRegister/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace userRegister.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        // GET: api/<RegistrationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var val = new string[] { "value1", "value2" };
+            Response.Headers.Add("Access-Control-Allow-Origin", "20");
+            return val;
+        }
+
+        // GET api/<RegistrationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RegistrationController>
+        [HttpPost]
+        public void Post([FromBody] string login, string password)
+        {
+            Console.WriteLine(login);
+        }
+
+        // PUT api/<RegistrationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RegistrationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
