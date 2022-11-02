0d042d58ce1b994913073e47f743cc1b08aaf627
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-11-02 19:29:15 +0300

    Revert "Fixed hash equals method."
    This reverts commit 391a7cab23219c87d9c4b61d7727ec1ed00a67b4.

--- API/Models/Service/Hash.cs
+++ API/Models/Service/Hash.cs
@@ -28,6 +28,24 @@ namespace API.Models.Service
             return Convert.ToBase64String(hashBytes);
         }
 
-        public bool Equals(string password, string hashedPassword) => HashString(password).Equals(hashedPassword, StringComparison.Ordinal);
+        public bool Equals(string password, string hashedPassword)
+        {
+            var hashBytes = Convert.FromBase64String(hashedPassword);
+
+            var salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations);
+            byte[] hash = pbkdf2.GetBytes(HashSize);
+
+            for (var i = 0; i < HashSize; i++)
+            {
+                if (hashBytes[i + SaltSize] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
